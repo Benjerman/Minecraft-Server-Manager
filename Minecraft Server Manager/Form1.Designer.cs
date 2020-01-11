b0daebd7ab2c2b5a743b48b068e78d9ce559fde0
ben nonn <bnonn@alicat.com>
2020-01-10 17:16:55 -0700

    Commiting local code to repository

---
+++ Minecraft Server Manager/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Minecraft_Server_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.txtInputCommand = new System.Windows.Forms.RichTextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.backupButton = new System.Windows.Forms.Button();
+            this.startServerButton = new System.Windows.Forms.Button();
+            this.stopServerButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.weatherComboBox = new System.Windows.Forms.ComboBox();
+            this.setWeatherButton = new System.Windows.Forms.Button();
+            this.opPlayerButton = new System.Windows.Forms.Button();
+            this.opPlayerTextBox1 = new System.Windows.Forms.TextBox();
+            this.deOpPlayerButton = new System.Windows.Forms.Button();
+            this.deOpTextBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.gameRuleComboBox = new System.Windows.Forms.ComboBox();
+            this.trueGRRadioButton = new System.Windows.Forms.RadioButton();
+            this.falseGRRadioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 238);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(628, 378);
+            this.txtOutput.TabIndex = 0;
+            this.txtOutput.Text = "";
+            // 
+            // txtInputCommand
+            // 
+            this.txtInputCommand.Location = new System.Drawing.Point(15, 196);
+            this.txtInputCommand.Name = "txtInputCommand";
+            this.txtInputCommand.Size = new System.Drawing.Size(625, 23);
+            this.txtInputCommand.TabIndex = 1;
+            this.txtInputCommand.Text = "";
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(646, 194);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 2;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(646, 12);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 23);
+            this.btnQuit.TabIndex = 3;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // backupButton
+            // 
+            this.backupButton.Location = new System.Drawing.Point(12, 121);
+            this.backupButton.Name = "backupButton";
+            this.backupButton.Size = new System.Drawing.Size(103, 42);
+            this.backupButton.TabIndex = 4;
+            this.backupButton.Text = "Backup World";
+            this.backupButton.UseVisualStyleBackColor = true;
+            this.backupButton.Click += new System.EventHandler(this.backupButton_Click);
+            // 
+            // startServerButton
+            // 
+            this.startServerButton.Location = new System.Drawing.Point(12, 25);
+            this.startServerButton.Name = "startServerButton";
+            this.startServerButton.Size = new System.Drawing.Size(103, 42);
+            this.startServerButton.TabIndex = 5;
+            this.startServerButton.Text = "Start Server";
+            this.startServerButton.UseVisualStyleBackColor = true;
+            this.startServerButton.Click += new System.EventHandler(this.startServerButton_Click);
+            // 
+            // stopServerButton
+            // 
+            this.stopServerButton.Location = new System.Drawing.Point(12, 73);
+            this.stopServerButton.Name = "stopServerButton";
+            this.stopServerButton.Size = new System.Drawing.Size(103, 42);
+            this.stopServerButton.TabIndex = 6;
+            this.stopServerButton.Text = "Stop Server";
+            this.stopServerButton.UseVisualStyleBackColor = true;
+            this.stopServerButton.Click += new System.EventHandler(this.stopServerButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Server Administration";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Server Output";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Enter Command";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(178, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Admin Commands";
+            // 
+            // weatherComboBox
+            // 
+            this.weatherComboBox.FormattingEnabled = true;
+            this.weatherComboBox.Location = new System.Drawing.Point(262, 26);
+            this.weatherComboBox.Name = "weatherComboBox";
+            this.weatherComboBox.Size = new System.Drawing.Size(121, 21);
+            this.weatherComboBox.TabIndex = 11;
+            this.weatherComboBox.SelectedIndexChanged += new System.EventHandler(this.weatherComboBox_SelectedIndexChanged);
+            // 
+            // setWeatherButton
+            // 
+            this.setWeatherButton.Location = new System.Drawing.Point(181, 25);
+            this.setWeatherButton.Name = "setWeatherButton";
+            this.setWeatherButton.Size = new System.Drawing.Size(75, 21);
+            this.setWeatherButton.TabIndex = 12;
+            this.setWeatherButton.Text = "Set Weather";
+            this.setWeatherButton.UseVisualStyleBackColor = true;
+            this.setWeatherButton.Click += new System.EventHandler(this.setWeatherButton_Click);
+            // 
+            // opPlayerButton
+            // 
+            this.opPlayerButton.Location = new System.Drawing.Point(181, 52);
+            this.opPlayerButton.Name = "opPlayerButton";
+            this.opPlayerButton.Size = new System.Drawing.Size(75, 21);
+            this.opPlayerButton.TabIndex = 13;
+            this.opPlayerButton.Text = "OP Player";
+            this.opPlayerButton.UseVisualStyleBackColor = true;
+            this.opPlayerButton.Click += new System.EventHandler(this.opPlayerButton_Click);
+            // 
+            // opPlayerTextBox1
+            // 
+            this.opPlayerTextBox1.Location = new System.Drawing.Point(262, 53);
+            this.opPlayerTextBox1.Name = "opPlayerTextBox1";
+            this.opPlayerTextBox1.Size = new System.Drawing.Size(121, 20);
+            this.opPlayerTextBox1.TabIndex = 14;
+            // 
+            // deOpPlayerButton
+            // 
+            this.deOpPlayerButton.Location = new System.Drawing.Point(181, 79);
+            this.deOpPlayerButton.Name = "deOpPlayerButton";
+            this.deOpPlayerButton.Size = new System.Drawing.Size(75, 21);
+            this.deOpPlayerButton.TabIndex = 15;
+            this.deOpPlayerButton.Text = "DeOp Player";
+            this.deOpPlayerButton.UseVisualStyleBackColor = true;
+            this.deOpPlayerButton.Click += new System.EventHandler(this.deOpPlayerButton_Click);
+            // 
+            // deOpTextBox1
+            // 
+            this.deOpTextBox1.Location = new System.Drawing.Point(262, 80);
+            this.deOpTextBox1.Name = "deOpTextBox1";
+            this.deOpTextBox1.Size = new System.Drawing.Size(121, 20);
+            this.deOpTextBox1.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(181, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Game Rule";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gameRuleComboBox
+            // 
+            this.gameRuleComboBox.FormattingEnabled = true;
+            this.gameRuleComboBox.Location = new System.Drawing.Point(262, 106);
+            this.gameRuleComboBox.Name = "gameRuleComboBox";
+            this.gameRuleComboBox.Size = new System.Drawing.Size(121, 21);
+            this.gameRuleComboBox.TabIndex = 18;
+            // 
+            // trueGRRadioButton
+            // 
+            this.trueGRRadioButton.AutoSize = true;
+            this.trueGRRadioButton.Location = new System.Drawing.Point(389, 109);
+            this.trueGRRadioButton.Name = "trueGRRadioButton";
+            this.trueGRRadioButton.Size = new System.Drawing.Size(47, 17);
+            this.trueGRRadioButton.TabIndex = 19;
+            this.trueGRRadioButton.TabStop = true;
+            this.trueGRRadioButton.Text = "True";
+            this.trueGRRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // falseGRRadioButton2
+            // 
+            this.falseGRRadioButton2.AutoSize = true;
+            this.falseGRRadioButton2.Location = new System.Drawing.Point(442, 109);
+            this.falseGRRadioButton2.Name = "falseGRRadioButton2";
+            this.falseGRRadioButton2.Size = new System.Drawing.Size(50, 17);
+            this.falseGRRadioButton2.TabIndex = 20;
+            this.falseGRRadioButton2.TabStop = true;
+            this.falseGRRadioButton2.Text = "False";
+            this.falseGRRadioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 628);
+            this.Controls.Add(this.falseGRRadioButton2);
+            this.Controls.Add(this.trueGRRadioButton);
+            this.Controls.Add(this.gameRuleComboBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.deOpTextBox1);
+            this.Controls.Add(this.deOpPlayerButton);
+            this.Controls.Add(this.opPlayerTextBox1);
+            this.Controls.Add(this.opPlayerButton);
+            this.Controls.Add(this.setWeatherButton);
+            this.Controls.Add(this.weatherComboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stopServerButton);
+            this.Controls.Add(this.startServerButton);
+            this.Controls.Add(this.backupButton);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.txtInputCommand);
+            this.Controls.Add(this.txtOutput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Minecraft Bedrock Server Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.RichTextBox txtInputCommand;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Button backupButton;
+        private System.Windows.Forms.Button startServerButton;
+        private System.Windows.Forms.Button stopServerButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox weatherComboBox;
+        private System.Windows.Forms.Button setWeatherButton;
+        private System.Windows.Forms.Button opPlayerButton;
+        private System.Windows.Forms.TextBox opPlayerTextBox1;
+        private System.Windows.Forms.Button deOpPlayerButton;
+        private System.Windows.Forms.TextBox deOpTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox gameRuleComboBox;
+        private System.Windows.Forms.RadioButton trueGRRadioButton;
+        private System.Windows.Forms.RadioButton falseGRRadioButton2;
+    }
+}
+
---
+++ Minecraft Server Manager/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Minecraft_Server_Manager
+{
+    public partial class Form1 : Form
+    {
+
+        Process minecraftProcess;
+        StreamWriter mcInputStream;
+
+
+        public delegate void fpTextBoxCallback_t(string strText);
+        public fpTextBoxCallback_t fpTextBoxCallback;
+
+
+        public Form1()
+        {
+            
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
+            fpTextBoxCallback = new fpTextBoxCallback_t(AddTextToOutputTextBox);
+            InitializeComponent();
+
+            List<string> weatherList = new List<string>();
+            weatherList.Add("clear");
+            weatherList.Add("rain");
+            weatherList.Add("thunder");
+
+            List<string> gameRuleList = new List<string>();
+            gameRuleList.Add("commandblockoutput");
+            gameRuleList.Add("commandblocksenabled");
+            gameRuleList.Add("dodaylightcycle");
+            gameRuleList.Add("doentitydrops");
+            gameRuleList.Add("dofiretick");
+            gameRuleList.Add("doimmediaterespawn");
+            gameRuleList.Add("doinsomnia");
+            gameRuleList.Add("domobloot");
+            gameRuleList.Add("domobspawning");
+            gameRuleList.Add("dotiledrops");
+            gameRuleList.Add("doweathercycle");
+            gameRuleList.Add("drowningdamage");
+            gameRuleList.Add("falldamage");
+            gameRuleList.Add("firedamage");
+            gameRuleList.Add("functioncommandlimit");
+            gameRuleList.Add("keepinventory");
+            gameRuleList.Add("maxcommandchainlength");
+            gameRuleList.Add("mobgriefing");
+            gameRuleList.Add("naturalregeneration");
+            gameRuleList.Add("pvp");
+            gameRuleList.Add("randomtickspeed");
+            gameRuleList.Add("sendcommandfeedback");
+            gameRuleList.Add("showcoordinates");
+            gameRuleList.Add("showdeathmessages");
+            gameRuleList.Add("tntexplodes");
+
+
+            weatherComboBox.DataSource = weatherList;
+
+            gameRuleComboBox.DataSource = gameRuleList;
+
+            
+        } // End Constructor
+
+
+        public void AddTextToOutputTextBox(string strText)
+        {
+            try
+            {
+                this.txtOutput.AppendText(strText);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        } 
+
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mcInputStream.WriteLine("stop");
+                Thread.Sleep(1000);
+                mcInputStream.Close();
+                minecraftProcess.Close();
+                minecraftProcess.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            Application.Exit();
+
+        } 
+
+
+        private void ConsoleOutputHandler(object sendingProcess, System.Diagnostics.DataReceivedEventArgs outLine)
+        {
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                if (this.InvokeRequired)
+                    this.Invoke(fpTextBoxCallback, Environment.NewLine + outLine.Data);
+                else
+                    fpTextBoxCallback(Environment.NewLine + outLine.Data);
+            }
+
+        }
+
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            if (this.minecraftProcess.HasExited)
+            {
+                MessageBox.Show("The server has been shutdown.", "Info");
+                return;
+            }
+
+            mcInputStream.WriteLine(txtInputCommand.Text);
+        }
+
+
+        public void ProcessExited(object sender, EventArgs e)
+        {
+            MessageBox.Show("The server has been shutdown.", "Info");
+        }
+
+
+        private void backupButton_Click(object sender, EventArgs e)
+        {
+            mcInputStream.WriteLine("say THE SERVER IS GOING DOWN FOR A BACKUP IN 10 SECONDS");
+            MessageBox.Show("Telling players the server is going down in 10 seconds, Please click ok to contiue with the backup");
+            Thread.Sleep(10000);
+            mcInputStream.WriteLine("stop");
+            Thread.Sleep(1000);
+            string source_dir = "";
+            string destination_dir = "";
+
+            source_dir = @"worlds";
+            destination_dir = @"backups\worlds" + DateTime.Now.ToString("hhmmttMMddyyyy");
+
+
+
+            foreach (string dir in System.IO.Directory.GetDirectories(source_dir, "*", System.IO.SearchOption.AllDirectories))
+            {
+                System.IO.Directory.CreateDirectory(System.IO.Path.Combine(destination_dir, dir.Substring(source_dir.Length + 1)));
+
+            }
+
+            foreach (string file_name in System.IO.Directory.GetFiles(source_dir, "*", System.IO.SearchOption.AllDirectories))
+            {
+                System.IO.File.Copy(file_name, System.IO.Path.Combine(destination_dir, file_name.Substring(source_dir.Length + 1)));
+            }
+
+            MessageBox.Show("Backup Complete. Please start the server again");
+
+
+        }
+
+        private void startServerButton_Click(object sender, EventArgs e)
+        {
+            string processFileName = "";
+
+
+            processFileName = @"bedrock_server.exe";
+
+
+            minecraftProcess = new System.Diagnostics.Process();
+
+            minecraftProcess.StartInfo.FileName = processFileName;
+
+            AddTextToOutputTextBox("Using this terminal: " + minecraftProcess.StartInfo.FileName);
+
+            minecraftProcess.StartInfo.UseShellExecute = false;
+            minecraftProcess.StartInfo.CreateNoWindow = true;
+            minecraftProcess.StartInfo.RedirectStandardInput = true;
+            minecraftProcess.StartInfo.RedirectStandardOutput = true;
+            minecraftProcess.StartInfo.RedirectStandardError = true;
+
+            minecraftProcess.EnableRaisingEvents = true;
+            minecraftProcess.Exited += new EventHandler(ProcessExited);
+            minecraftProcess.ErrorDataReceived += new System.Diagnostics.DataReceivedEventHandler(ConsoleOutputHandler);
+            minecraftProcess.OutputDataReceived += new System.Diagnostics.DataReceivedEventHandler(ConsoleOutputHandler);
+
+            minecraftProcess.Start();
+
+            mcInputStream = minecraftProcess.StandardInput;
+            minecraftProcess.BeginOutputReadLine();
+            minecraftProcess.BeginErrorReadLine();
+        }
+
+        private void stopServerButton_Click(object sender, EventArgs e)
+        {
+            mcInputStream.WriteLine("stop");
+        }
+
+        private void OnProcessExit(object sender, EventArgs e)
+        {
+            mcInputStream.WriteLine("stop");
+            Thread.Sleep(5000);
+        }
+
+        private void weatherComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setWeatherButton_Click(object sender, EventArgs e)
+        {            
+            mcInputStream.WriteLine("weather " + weatherComboBox.SelectedItem);
+        }
+
+        private void opPlayerButton_Click(object sender, EventArgs e)
+        {
+            mcInputStream.WriteLine("op " + opPlayerTextBox1.Text.ToString());
+        }
+
+        private void deOpPlayerButton_Click(object sender, EventArgs e)
+        {
+            mcInputStream.WriteLine("deop " + deOpTextBox1.Text.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string trueFalse = " false";
+            if (trueGRRadioButton.Checked)
+                trueFalse = " true";
+            if (falseGRRadioButton2.Checked)
+                trueFalse = " false";
+
+            mcInputStream.WriteLine("gamerule " + gameRuleComboBox.SelectedItem + trueFalse);
+        }
+    }
+
+
+}
